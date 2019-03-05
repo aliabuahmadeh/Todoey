62409b0a6156fd2150689880b0a90b407c3fd8ba
Ali Abuahmadeh <aliabuahmadeh@icloud.com>
2019-03-05 21:21:49 +0300

    convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by ALI ABU AHMADEH on 05/03/2019.
+//  Copyright © 2019 ALI ABU AHMADEH. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var done = false
+}
